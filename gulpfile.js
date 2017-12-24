cc72f0c4aef9bcc4435eeed2cada9ab51c26b81a
EnderDFox <232623741@qq.com>
2017-12-24 16:16:18 +0800

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var gulp = require("gulp");
 var child_process = require("child_process");
 gulp.task('AutoReferencePath src', function (cb) {
-    child_process.exec("node C:/fox/projects/tools/NodeJsTools/bin/js/AutoReferencePath.js -d src -o GameMain.ts Main.ts", function (err) {
-        if (err)
-            return cb(err);
+    var free = child_process.spawn('node', ['C:/fox/projects/tools/NodeJsTools/bin/js/AutoReferencePath.js',
+        '-d', 'src', '-e', 'GameMain.ts', 'Main.ts']);
+    free.stdout.on('data', function (data) {
+        console.log('' + data);
+    });
+    free.stderr.on('data', function (data) {
+        console.log('standard error output:\n' + data);
+    });
+    free.on('exit', function (code, signal) {
+        console.log('child process eixt ,exit:' + code);
         cb();
     });
 });
--- gulpfile.ts
+++ gulpfile.ts
@@ -3,8 +3,16 @@ import * as ts from 'gulp-typescript';
 import * as child_process from 'child_process';
 
 gulp.task('AutoReferencePath src', function (cb) {
-    child_process.exec("node C:/fox/projects/tools/NodeJsTools/bin/js/AutoReferencePath.js -d src -o GameMain.ts Main.ts", function (err) {
-        if (err) return cb(err);
+    var free = child_process.spawn('node', ['C:/fox/projects/tools/NodeJsTools/bin/js/AutoReferencePath.js',
+        '-d', 'src', '-e', 'GameMain.ts', 'Main.ts']);
+    free.stdout.on('data', function (data) {
+        console.log('' + data);
+    });
+    free.stderr.on('data', function (data) {
+        console.log('standard error output:\n' + data);
+    });
+    free.on('exit', function (code, signal) {
+        console.log('child process eixt ,exit:' + code);
         cb();
     });
 });
