ca5d4a84b48d06ae89159f38b9a425993eb1696f
232623741@qq.com <232623741@qq.com>
2018-01-30 02:51:38 +0800

    change map size

--- src/ctrls/tools/MapEditorCtrl.ts
+++ src/ctrls/tools/MapEditorCtrl.ts
@@ -60,17 +60,17 @@ namespace tools {
             request.once(egret.Event.COMPLETE, this.openMapRespHandler.bind(this), null);
             request.once(egret.IOErrorEvent.IO_ERROR, this.openMapRespHandler.bind(this), null);
             request.once(egret.ProgressEvent.PROGRESS, progressHandler.bind(this), null);
-            var url = StcMap.mapPath(sid)+ `?r=${Math.random()}`;
+            var url = StcMap.mapPath(sid) + `?r=${Math.random()}`;
             request.open(url, egret.HttpMethod.GET);
             request.send();
         }
         openMapRespHandler(evt: egret.Event): void {
             switch (evt.type) {
                 case egret.Event.COMPLETE:
-                var request: egret.HttpRequest = evt.currentTarget;
-                // console.log("respHandler:n", request.response);
-                this.openMapJsonStr(request.response);
-                break;
+                    var request: egret.HttpRequest = evt.currentTarget;
+                    // console.log("respHandler:n", request.response);
+                    this.openMapJsonStr(request.response);
+                    break;
                 case egret.IOErrorEvent.IO_ERROR:
                     console.log("respHandler io error");
                     break;
@@ -86,63 +86,76 @@ namespace tools {
             this.onBtnSetSize();
             for (let i = 0; i < this.cells.length; i++) {
                 for (let j = 0; j < this.cells[0].length; j++) {
-                    let cell:UI_MapCell = this.cells[i][j];
+                    let cell: UI_MapCell = this.cells[i][j];
                     cell.m_kind.selectedIndex = mapVo.cells[i][j];
                 }
             }
         }
         onBtnSave() {
-            let sid:number = parseInt(this.ui.m_txtMapId.text);
-            let mapVo:IStcMapVo = {kind:StcMapKind.Kind1,cells:[]};
+            let sid: number = parseInt(this.ui.m_txtMapId.text);
+            let mapVo: IStcMapVo = { kind: StcMapKind.Kind1, cells: [] };
             mapVo.version = StcMapVersion.v1;
             mapVo.sid = sid;
-            mapVo.positions=[];
+            mapVo.positions = [];
             for (let i = 0; i < this.cells.length; i++) {
                 mapVo.cells[i] = []
                 for (let j = 0; j < this.cells[0].length; j++) {
-                    let cell:UI_MapCell = this.cells[i][j];
+                    let cell: UI_MapCell = this.cells[i][j];
                     mapVo.cells[i][j] = cell.m_kind.selectedIndex;
                 }
             }
             let jsonStr = JSON.stringify(mapVo);
-            console.log("[debug]",jsonStr,"`jsonStr`");
+            console.log("[debug]", jsonStr, "`jsonStr`");
             //---
             var params = `?file=${StcMap.mapPath(sid)}&content=${jsonStr}`;
             var request = new egret.HttpRequest();
             request.responseType = egret.HttpResponseType.TEXT;
-            request.open("savefile"+params,egret.HttpMethod.GET);
+            request.open("savefile" + params, egret.HttpMethod.GET);
             request.send();
         }
         onBtnSetSize() {
             let col = parseInt(this.ui.m_txtCol.text);
-            col = MathUtil.clamp(isNaN(col)?1:col,1,200);
+            col = MathUtil.clamp(isNaN(col) ? 1 : col, 1, 200);
             let row = parseInt(this.ui.m_txtRow.text);
-            row = MathUtil.clamp(isNaN(row)?1:row,1,200);
+            row = MathUtil.clamp(isNaN(row) ? 1 : row, 1, 200);
             //-
             this.ui.m_mapArea.width = models.fights.FightModelUtil.gridToPos(col);
             this.ui.m_mapArea.height = models.fights.FightModelUtil.gridToPos(row);
-            for (let i = 0; i < this.cells.length; i++) {
-                for (let j = 0; j < this.cells[i].length; j++) {
-                    this.cells[i][j].dispose();
+            for (let i = 0; i < Math.min(this.cells.length, col); i++) {
+                for (let j = this.cells[i].length; j < row; j++) {
+                    this.addCell(i, j);
                 }
-                delete this.cells[i];
+                for (let j = row; j < this.cells[i].length; j++) {
+                    this.cells[i][j].dispose();
+                    this.cells[i].splice(j, 1);
+                    j--;
+                }
             }
-            this.cells = [];
             for (let i = this.cells.length; i < col; i++) {
                 this.cells[i] = [];
                 for (let j = 0; j < row; j++) {
-                    let cell: UI_MapCell = UI_MapCell.createInstance();
-                    this.cells[i][j] = cell;
-                    cell.setXY(models.fights.FightModelUtil.gridToPos(i), models.fights.FightModelUtil.gridToPos(j));
-                    this.ui.m_mapArea.addChild(cell);
-                    cell.m_kind.selectedIndex = StcCellSid.floor;
+                    this.addCell(i, j);
                 }
+            }
+            for (let i = col; i < this.cells.length; i++) {
+                for (let j = 0; j < this.cells[i].length; j++) {
+                    this.cells[i][j].dispose();
+                }
+                this.cells.splice(i, 1);
+                i--;
             }
             //
             let scale = Math.min(1,
                 (this.ui.width - this.ui.m_mapArea.x - 10) / this.ui.m_mapArea.width,
                 (this.ui.height - this.ui.m_mapArea.y - 10) / this.ui.m_mapArea.height);
             this.ui.m_mapArea.scaleX = this.ui.m_mapArea.scaleY = scale;
+        }
+        private addCell(col: number, row: number) {
+            let cell: UI_MapCell = UI_MapCell.createInstance();
+            this.cells[col][row] = cell;
+            cell.setXY(models.fights.FightModelUtil.gridToPos(col), models.fights.FightModelUtil.gridToPos(row));
+            this.ui.m_mapArea.addChild(cell);
+            cell.m_kind.selectedIndex = StcCellSid.floor;
         }
         private getMapSid(): number {
             return parseInt(this.ui.m_txtMapId.text);
@@ -166,14 +179,7 @@ namespace tools {
                 var col = models.fights.FightModelUtil.posToGrid(tempXY.x);
                 var row = models.fights.FightModelUtil.posToGrid(tempXY.y);
                 if (this.cells[col] && this.cells[col][row]) {
-                    if (this.currOverCell != null) {
-                        this.currOverCell.m_crack.m_lv.selectedIndex = 0;
-                        this.currOverCell.filters = null;
-                        this.currOverCell = null;
-                    }
-                    this.currOverCell = this.cells[col][row];
-                    this.currOverCell.m_crack.m_lv.selectedIndex = 2;
-                    this.currOverCell.filters = [new egret.GlowFilter(0xFFFF00, 0.8, 12, 12, 2)];
+                    this.setCurrOverCell(this.cells[col][row]);
                 }
                 if (KeyBoardCtrl.si.altKey) {
                     this.setCellSidSafe(col, row, StcCellSid.floor);
@@ -212,7 +218,19 @@ namespace tools {
             }
         }
         private onMapArea_TouchEnd(e: egret.TouchEvent) {
-
+            this.setCurrOverCell(null);
+        }
+        private setCurrOverCell(cell: UI_MapCell) {
+            if (this.currOverCell != null) {
+                this.currOverCell.m_crack.m_lv.selectedIndex = 0;
+                this.currOverCell.filters = null;
+                this.currOverCell = null;
+            }
+            if (cell) {
+                this.currOverCell = cell;
+                this.currOverCell.m_crack.m_lv.selectedIndex = 2;
+                this.currOverCell.filters = [new egret.GlowFilter(0xFFFF00, 0.8, 12, 12, 2)];
+            }
         }
         private onMouseWheelChange(delta: number) {
             this.ui.m_mapArea.scaleX = this.ui.m_mapArea.scaleY = this.ui.m_mapArea.scaleX + delta / 1000;
